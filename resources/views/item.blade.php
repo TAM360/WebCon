1eca160f9648acb161e5301291502168a155e6a4
TAM360 <demon6656@gmail.com>
2019-01-15 13:15:25 +0500

    searchProductsController updated

---
+++ resources/views/item.blade.php
@@ -0,0 +1,308 @@
+<html lang="en-US">
+
+<head>
+    <title>Construction – Construction Site</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" id="font-awesome-css" href="https://demo.accesspressthemes.com/construction/wp-content/themes/construction-lite/css/font-awesome/css/font-awesome.min.css?ver=4.9.1" type="text/css" media="all">
+    <link rel="stylesheet" id="construction-lite-style-css" href="https://demo.accesspressthemes.com/construction/wp-content/themes/construction-lite/style.css?ver=4.9.1" type="text/css" media="all">
+    <link rel="stylesheet" id="owl-carousel-css" href="https://demo.accesspressthemes.com/construction/wp-content/themes/construction-lite/js/owl-carousel/owl.carousel.css?ver=4.9.1" type="text/css" media="all">
+    <link rel="stylesheet" id="construction-lite-woocommerce-style-css" href="https://demo.accesspressthemes.com/construction/wp-content/themes/construction-lite/woocommerce/woocommerce-style.css?ver=4.9.1" type="text/css" media="all">
+    <link rel="stylesheet" id="construction-lite-responsive-css" href="https://demo.accesspressthemes.com/construction/wp-content/themes/construction-lite/responsive.css?ver=4.9.1" type="text/css" media="all">
+    <link rel="stylesheet" id="animate-css" href="https://demo.accesspressthemes.com/construction/wp-content/themes/construction-lite/js/wow-animation/animate.css?ver=4.9.1" type="text/css" media="all">
+    <script type="text/javascript" src="https://demo.accesspressthemes.com/construction/wp-includes/js/jquery/jquery.js?ver=1.12.4"></script>
+    <script type="text/javascript" src="https://demo.accesspressthemes.com/construction/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1"></script>
+    <script type="text/javascript" src="https://demo.accesspressthemes.com/construction/wp-content/plugins/accesspress-twitter-feed/js/jquery.bxslider.min.js?ver=1.5.6"></script>
+    <script type="text/javascript" src="https://demo.accesspressthemes.com/construction/wp-content/plugins/accesspress-twitter-feed/js/frontend.js?ver=1.5.6"></script>
+    <script type="text/javascript" src="https://demo.accesspressthemes.com/construction/wp-content/themes/construction-lite/js/wow-animation/wow.js?ver=4.9.1"></script>
+    <script type="text/javascript" src="https://demo.accesspressthemes.com/construction/wp-content/themes/construction-lite/js/parallax.js?ver=4.9.1"></script>
+    <script type="text/javascript" src="https://demo.accesspressthemes.com/construction/wp-content/themes/construction-lite/js/owl-carousel/owl.carousel.js?ver=4.9.1"></script>
+    <script type="text/javascript" src="https://demo.accesspressthemes.com/construction/wp-content/themes/construction-lite/js/custom.js?ver=4.9.1"></script>
+    <link rel="https://api.w.org/" href="https://demo.accesspressthemes.com/construction/wp-json/">
+    <link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://demo.accesspressthemes.com/construction/xmlrpc.php?rsd">
+    <link rel="wlwmanifest" type="application/wlwmanifest+xml" href="https://demo.accesspressthemes.com/construction/wp-includes/wlwmanifest.xml">
+    <meta name="generator" content="WordPress 4.9.1">
+    <meta name="generator" content="WooCommerce 3.2.5">
+    <link rel="canonical" href="https://demo.accesspressthemes.com/construction/">
+    <link rel="shortlink" href="https://demo.accesspressthemes.com/construction/">
+    <link rel="alternate" type="application/json+oembed" href="https://demo.accesspressthemes.com/construction/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fdemo.accesspressthemes.com%2Fconstruction%2F">
+    <link rel="alternate" type="text/xml+oembed" href="https://demo.accesspressthemes.com/construction/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fdemo.accesspressthemes.com%2Fconstruction%2F&amp;format=xml">
+    <noscript><style>.woocommerce-product-gallery{ opacity: 1 !important; }</style></noscript>
+    <style type="text/css">
+        .recentcomments a {
+            display: inline !important;
+            padding: 0 !important;
+            margin: 0 !important;
+        }
+    </style>
+    <link rel="icon" href="https://demo.accesspressthemes.com/construction/wp-content/uploads/2016/10/site-icon.jpg" sizes="32x32">
+    <link rel="icon" href="https://demo.accesspressthemes.com/construction/wp-content/uploads/2016/10/site-icon.jpg" sizes="192x192">
+    <link rel="apple-touch-icon-precomposed" href="https://demo.accesspressthemes.com/construction/wp-content/uploads/2016/10/site-icon.jpg">
+    <meta name="msapplication-TileImage" content="https://demo.accesspressthemes.com/construction/wp-content/uploads/2016/10/site-icon.jpg">
+    <style>
+        .cta_section {
+            background: url('https://demo.accesspressthemes.com/construction/wp-content/uploads/2016/09/buildings-1149917_1920.jpg') no-repeat center fixed;
+        }
+
+        .main-navigation .current_page_item>a,
+        .main-navigation .current-menu-item>a,
+        .main-navigation a:hover,
+        .main-navigation li:hover>a,
+        .main-navigation li.focus>a,
+        .header-cart-search:hover .cart-fa-icon,
+        a:focus,
+        a:active,
+        .member-social-profile a:hover,
+        .site-header .search-icon:hover,
+        .blog_section .blogs-loop a:hover,
+        .woocommerce .star-rating,
+        .woocommerce .star-rating::before,
+        .item-wrap a.product-name h5:hover,
+        .item-wrap .price,
+        .woocommerce ul.products li.product .price,
+        .site-footer .site-info a:hover,
+        .bottom-footer a:hover,
+        .bottom-footer .widget_construction_lite_recent_post .recent-posts-content a:hover,
+        .woocommerce .product-rating,
+        .woocommerce ul.products li.product .star-rating,
+        .widget a:hover,
+        .woocommerce-info::before,
+        .comment-author-date a:hover,
+        .entry-title a:hover,
+        .comments-area .reply .comment-reply-link,
+        .woocommerce.widget_shopping_cart ul.cart_list li a:hover,
+        .product-name a:hover,
+        .product-name a:focus,
+        .product-name a:active,
+        .about-post-title a:hover,
+        .edit-link a:hover,
+        .edit-link a:focus,
+        .edit-link a:active,
+        .top-footer .social-icons .fa_link_wrap a:hover .fa_wrap,
+        #construction-breadcrumb a,
+        .portfolio_section .owl-controls .owl-nav .owl-prev:hover:before,
+        .portfolio_section .owl-controls .owl-nav .owl-next:hover:before,
+        .testimonial_section .top-quote:before,
+        .testimonial_section .bottom-quote:before,
+        a:hover,
+        a:focus,
+        a:active,
+        .comments-area a:hover,
+        .comments-area .comment-author .fn a:hover,
+        .woocommerce div.product p.price,
+        .woocommerce div.product span.price,
+        .woocommerce #reviews .comment-form-rating p.stars a {
+            color: #FEA100;
+        }
+
+        .widget_aptf_widget .aptf-tweet-content .aptf-tweet-name,
+        .site-footer .site-info a {
+            color: #FEA100 !important;
+        }
+
+        .woocommerce a.remove,
+        .woocommerce.widget_shopping_cart ul.cart_list li a.remove {
+            color: #FEA100!important;
+        }
+
+        .cart-count,
+        .woocommerce a.remove:hover,
+        .woocommerce.widget_shopping_cart ul.cart_list li a.remove:hover,
+        .site-header .ak-search input[type="submit"],
+        .slider-content a:hover,
+        .section-sub-title h2::before,
+        .about-button a:hover,
+        .feature_section .posts-feature,
+        .member-name-designation-social .member-designation::after,
+        .blog_section .blog-left .blog-date,
+        .item-wrap .add-to-cart-shop a:hover,
+        .blog_section .blogs-loop .blog-title::after,
+        .title-cta::after,
+        .cta-button a,
+        .test-psots-wrap .owl-controls .owl-dot:hover,
+        .test-psots-wrap .owl-controls .owl-dot.active,
+        .bottom-footer .widget-title::after,
+        .mail-slider-header-wrap .owl-prev:hover,
+        .mail-slider-header-wrap .owl-next:hover,
+        .archive.woocommerce a.button,
+        .woocommerce nav.woocommerce-pagination ul li a:hover,
+        .widget-title::after,
+        .comments-area .comment-reply-title::after,
+        .comments-area .comments-title::after,
+        .widget_tag_cloud .tagcloud a:hover,
+        .woocommerce-MyAccount-navigation ul li::before,
+        .woocommerce .cart .button,
+        .woocommerce .cart input.button,
+        .woocommerce #payment #place_order,
+        .woocommerce-page #payment #place_order,
+        .woocommerce input.button,
+        article.post .entry-title::after,
+        article.post a.read-more,
+        .nav-links .page-numbers.current,
+        .nav-links a.page-numbers:hover,
+        .navigation a,
+        .comments-area .reply .comment-reply-link:hover,
+        .comments-area input[type="submit"],
+        .page-content .search-form input[type="submit"],
+        .woocommerce-cart .wc-proceed-to-checkout a.checkout-button:hover,
+        .contact-area .contact-form-post input[type="submit"],
+        .woocommerce a.button,
+        .woocommerce a.added_to_cart,
+        .calendar_wrap caption,
+        .slider-content div span a,
+        .woocommerce div.product form.cart .button,
+        .woocommerce #review_form #respond .form-submit input,
+        .woocommerce div.product form.cart .button,
+        .woocommerce #review_form #respond .form-submit input,
+        .woocommerce input.button:disabled:hover,
+        .woocommerce input.button:disabled[disabled]:hover {
+            background-color: #FEA100;
+        }
+
+        .woocommerce-MyAccount-navigation ul li {
+            border-left: 3px solid #FEA100;
+        }
+
+        .woocommerce-info {
+            border-top-color: #FEA100;
+        }
+
+        .navigation a {
+            border: 2px solid #FEA100;
+        }
+
+        .site-header .ak-search input[type="submit"],
+        .slider-content a:hover,
+        .about-button a:hover,
+        .cta-button a,
+        .woocommerce nav.woocommerce-pagination ul li a:hover,
+        .nav-links .page-numbers.current,
+        .nav-links a.page-numbers:hover,
+        .comments-area .reply .comment-reply-link:hover,
+        .comments-area input[type="submit"],
+        .page-content .search-form input[type="submit"],
+        .woocommerce-cart .wc-proceed-to-checkout a.checkout-button:hover,
+        .contact-area .contact-form-post input[type="submit"],
+        .top-footer .social-icons .fa_link_wrap a:hover .fa_wrap,
+        .slider-content div span a {
+            border-color: #FEA100;
+        }
+
+        .woocommerce input.button:disabled,
+        .woocommerce input.button:disabled[disabled] {
+            background: rgba(254, 161, 0, 0.5);
+        }
+    </style>
+</head>
+
+<body class="home page-template page-template-template-parts page-template-construction-home page-template-template-partsconstruction-home-php page page-id-22 wp-custom-logo">
+
+
+    <header id="masthead" class="site-header" role="banner">
+        @include('layouts.menubar')
+    </header>
+
+    <div class="container-fluid">
+        <center>
+            <h2 style="color:brown"> <b>ITEM DESCRIPTION</b> </h2>
+        </center>
+        <br>
+
+        {{-- <div style="background-color:saddlebrown;color:white;padding:3px; font-size: 15px;">
+            <div class="row">
+                <div class="col-md-3">
+
+                    <p>ITEM DESCRIPTION</p>
+                </div>
+
+                <div class="col-md-8">
+
+                </div>
+
+                <div class="col-md-1">
+
+                    <p>PRICE</p>
+                </div>
+            </div>
+        </div>
+
+        <br>
+ --}}
+
+        <div class="productdis" style="font-size: 15px;">
+            <div class="row myrow">
+                <div class="col-md-3">
+                    <div class="product">
+                        <img src="https://www.stockportfencing.co.uk/wp-content/uploads/2016/11/s202.jpg">
+                    </div>
+                </div>
+                <div class="col-md-7">
+
+                    <br>
+                    <br>
+
+                    <p><b><u>PRODUCT TITLE:      CASTLE CEMENT</u></b></p>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lacus purus, posuere eu venenatis et, ornare id mi. Proin laoreet posuere elit. Duis diam eros, fermentum non elit vitae, condimentum accumsan turpis. Morbi volutpat, augue
+                        ac pretium dictum, libero lorem mollis sem, eget elementum sapien justo eu ipsum. Pellentesque interdum sapien nec leo tincidunt ornare. Etiam sit amet justo a felis tincidunt faucibus quis ut quam. Aliquam erat volutpat. Phasellus
+                        nulla lorem, sagittis et mi eu, egestas suscipit enim. Curabitur rutrum elementum dui, a convallis dolor iaculis a. Phasellus vitae tortor venenatis, molestie felis sed, suscipit massa. Pellentesque gravida sollicitudin.</p>
+
+                </div>
+                <div class="col-md-2">
+                    <br>
+                    <br>
+                    <p style="color:saddlebrown"><b>PKR 10500.00</b></p>
+
+                    <div class="pdis marg"><b>Quantity</b></div>
+                    <div class="qminus" style="background-color: #C27760;
+                        color: #F2ECE4;
+                        width:30px;
+                        height: 30px;
+                        background-color: #F2ECE4;
+                        color: #C27760;
+                        display: inline-block;
+                        cursor:pointer;
+                        margin: 2px 0; 
+                        margin-right: 5px;
+                        padding-top: 5px;
+                        margin-top: 10px;
+                        font-weight: 600;">
+                        <center>-</center>
+                    </div>
+                    <div class="qnum" style="background-color: #C27760; color: #F2ECE4;width:50px;height: 30px;background-color: #F2ECE4; color: #C27760;display: inline-block;margin: 2px 0;margin-right: 5px;padding-top: 5px;margin-top: 10px;text-align: center;">
+                        <center>1</center>
+                    </div>
+                    <div class="qplus" style="background-color: #C27760;
+                        color: #F2ECE4;
+                        width:30px;
+                        height: 30px;
+                        background-color: #F2ECE4;
+                        color: #C27760;
+                        display: inline-block;
+                        cursor:pointer;
+                        margin: 2px 0; 
+                        margin-right: 5px;
+                        padding-top: 5px;
+                        margin-top: 10px;
+                        font-weight: 600;">
+                        <center>+</center>
+                    </div>
+                    <input type="hidden" id="q-value" name="q-value" value="1" />
+                    <button type="submit" style="background-color:#FEA100; border-radius:0;padding:10px;font-size:14px;">REMOVE</button>
+                    <a href="Checkout.html"> <button type="submit" style="background-color:#FEA100; border-radius:0;padding:10px;font-size:14px;">CheckOut</button> </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer id="colophon" class="site-footer" role="contentinfo">
+        @include('layouts.footer')
+    </footer>
+</body>
+
+</html>
